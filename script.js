2159b9513b0d96b64508d15cb3a51ac6c9027301
Jennifer Zheng <jez38@pitt.edu>
2021-03-14 15:40:38 -0400

    Gets placeID for departure location

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const REACT_APP_API_KEY = "902bd3bd07msh3bf09281e0f8dd9p1d823cjsn1cfe883a7323";
 
-let depart_city;
-let arrive_city;
+let depart_loc;
+let arrive_loc;
 let currency;
 let depart_date;
 let return_date;
+let placeID;
 
-async function fetchAPI() {
-  console.log("in fetchAPI function " + depart_city);
+async function getPlace() {
   const reqOptions = {
     method: "GET",
     headers: {
@@ -19,31 +19,48 @@ async function fetchAPI() {
   };
 
   let response = await fetch(
-    "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/" + currency + "/en-US/?" +
-      new URLSearchParams({ query: depart_city }),
+    "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/" +
+      currency +
+      "/en-US/?" +
+      new URLSearchParams({ query: depart_loc }),
     reqOptions
   )
-  .then(response => {console.log(response);})
-  .catch(err=>{console.error(err)});
+    .then(response => response.json())
+    .then(places => {
+      placeID = places.Places[0].PlaceId;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+  return;
 }
 
-function getParams() {
+async function getParams() {
   event.preventDefault();
-  depart_city = "";
-  arrive_city = "anywhere";
+
+  //default for parameters
+  depart_loc = "";
+  arrive_loc = "anywhere";
   currency = "USD";
   depart_date = "anytime";
   return_date = "anytime";
-  
-  depart_city = document.getElementById("depart_city").value;
-  arrive_city = document.getElementById("arrive_city").value;
-  currency = document.getElementById("currency").value;
+
+  //get inputs if they not blank
+  if (document.getElementById("depart_loc").value != "") {
+    depart_loc = document.getElementById("depart_loc").value;
+  }
+  if (document.getElementById("arrive_loc").value != "") {
+    arrive_loc = document.getElementById("arrive_loc").value;
+  }
+  if (document.getElementById("currency").value != "") {
+    currency = document.getElementById("currency").value;
+  }
   depart_date = document.getElementById("depart_date").value;
   return_date = document.getElementById("return_date").value;
-  
-  console.log("in param function " + depart_city);
-  fetchAPI();
+
+  await getPlace(); //wait for this function to finish before continuin; prevents placeID = undefined
+  console.log("PlaceID2: " + placeID);
+
+  document.getElementById("depart_loc").value = ""; //clear input field
 }
-
-
-
